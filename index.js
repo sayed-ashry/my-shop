0c2cbae2d76ada700bd82f97ac811b1851535d03
sayed-ashry <139576591+sayed-ashry@users.noreply.github.com>
2023-09-01 15:10:23 +0300

    convert to rest api project

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
 import express from "express";
-import dotenv from "dotenv";
-import { connect } from "mongoose";
-import routes from "./routes/index.js";
 
 const app = express();
 
-dotenv.config();
-
-app.set("view engine", "ejs");
-
-app.set("views", "views");
-
-app.use(express.static("public"));
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
-
-app.use("*", (req, res) => {
-  res.render("notFound");
+app.use("/", (req, res) => {
+  res.json("Hello World!");
 });
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).render("errpage");
-});
-
-try {
-  await connect(process.env.DB_URI);
-  app.listen(process.env.PORT);
-} catch (err) {
-  console.error(
-    "An error occurred while connecting to the MongoDB database or starting the server"
-  );
-}
-
-export default app;
+app.listen(3000);
